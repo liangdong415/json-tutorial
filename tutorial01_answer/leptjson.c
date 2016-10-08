1154fc744e9d5446a17eed265d5da9352a3c5fe6
liangdong415 <liangdongnju@163.com>
2016-10-08 19:23:21 +0800

    add parse number in tutorial01_answer

--- tutorial01_answer/leptjson.c
+++ tutorial01_answer/leptjson.c
@@ -1,13 +1,18 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
-#include <stdlib.h>  /* NULL */
+#include <stdlib.h>  /* NULL strtod() */
+#include <errno.h> /* errno */
+#include <math.h> /* HUGE_VAL */
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)			((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)		((ch) >= '1' && (ch) <= '9')
 
 typedef struct {
     const char* json;
 }lept_context;
 
+/* static函数的意思是指，该函数只作用于编译单元中，那么没有被调用时，编译器可以发现*/
 static void lept_parse_whitespace(lept_context* c) {
     const char *p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
@@ -15,40 +20,54 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
-static int lept_parse_true(lept_context* c, lept_value* v) {
-    EXPECT(c, 't');
-    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_TRUE;
-    return LEPT_PARSE_OK;
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type){
+	EXPECT(c, literal[0]);
+	size_t i;
+	for(i = 0; literal[i + 1]; ++i){
+		if(c->json[i] != literal[i + 1])
+			return LEPT_PARSE_INVALID_VALUE;
+	}
+	c->json += i;
+	v->type = type;
+	return LEPT_PARSE_OK;
 }
 
-static int lept_parse_false(lept_context* c, lept_value* v) {
-    EXPECT(c, 'f');
-    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 4;
-    v->type = LEPT_FALSE;
-    return LEPT_PARSE_OK;
-}
-
-static int lept_parse_null(lept_context* c, lept_value* v) {
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_NULL;
-    return LEPT_PARSE_OK;
+static int lept_parse_number(lept_context* c, lept_value* v){
+	const char* p = c->json;
+	/* validate number */
+	if(*p == '-') ++p;
+	if(*p == '0') ++p;
+	else{
+		if(!ISDIGIT1TO9(*p)) return LEPT_PARSE_INVALID_VALUE;
+		for(++p; ISDIGIT(*p); ++p);
+	}
+	if(*p == '.'){
+		++p;
+		if(!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+		for(++p; ISDIGIT(*p); ++p);
+	}
+	if(*p == 'E' || *p == 'e'){
+		++p;
+		if(*p == '+' || *p == '-') ++p;
+		if(!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+		for(++p; ISDIGIT(*p); ++p);
+	}
+	errno = 0;
+	v->num = strtod(c->json, NULL);
+	if(errno == ERANGE && (v->num == HUGE_VAL || v->num == -HUGE_VAL))
+		return LEPT_PARSE_NUMBER_TOO_BIG;
+	c->json = p;
+	v->type = LEPT_NUMBER;
+	return LEPT_PARSE_OK;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-        case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_literal(c, v, "true", LEPT_TRUE);
+        case 'f':  return lept_parse_literal(c, v, "false", LEPT_FALSE);
+		case 'n':  return lept_parse_literal(c, v, "null", LEPT_NULL);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
+        default:   return lept_parse_number(c, v);
     }
 }
 
@@ -61,8 +80,10 @@ int lept_parse(lept_value* v, const char* json) {
     lept_parse_whitespace(&c);
     if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
         lept_parse_whitespace(&c);
-        if (*c.json != '\0')
+        if (*c.json != '\0'){
+    		v->type = LEPT_NULL;
             ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+		}
     }
     return ret;
 }
@@ -70,4 +91,9 @@ int lept_parse(lept_value* v, const char* json) {
 lept_type lept_get_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
+}
+
+double lept_get_number(const lept_value* v){
+	assert(v != NULL && v->type == LEPT_NUMBER);
+	return v->num;
 }
